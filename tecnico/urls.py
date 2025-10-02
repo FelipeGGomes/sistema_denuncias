da7218a067d3a2adccf100976a1c0ea1de0ec68e
Felipe Gomes <felipe.gomes@SUDEMAPB.INTRA>
2025-10-02 11:56:49 -0300

    Configuração da views para visualizar denuncias não atribuidas

--- tecnico/urls.py
+++ tecnico/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'tecnico'
 
 urlpatterns = [
-    path('dashboard/', views.tecnico_ver, name='tecnico_list'),
+    path('dashboard/', views.denuncia_list, name='tecnico_list'),
+    path("denuncia/<str:protocolo>/", views.denuncia_detail, name="denuncia_detail"),
 ]
--- tecnico/views.py
+++ tecnico/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Tecnico
-from denuncia.models import Denuncia, Logdenuncia
+from denuncias.models import Denuncia, LogDenuncia
 
 def denuncia_list(request):
     denuncias = Denuncia.objects.filter(status='NÃO_ATRIBUIDO')
     return render(request, 'dashboard.html', {'denuncias': denuncias})
+    
 
+def denuncia_detail(request, protocolo):
+    denuncia = get_object_or_404(Denuncia, protocolo=protocolo)
+    logs = denuncia.logs.all().order_by('-data_acao')
+
+    return render(resquest, 'denuncia_detail.html', {
+        "denuncia": denuncia,
+        "logs": logs,
+    })
 
 def tecnico_ver(request):
     return render(request, 'dashboard.html')
