76045295520d27eaa13a4532b3fa02326d5b971c
Felipe Gomes <felipe.gomes@SUDEMAPB.INTRA>
2025-10-03 09:58:42 -0300

    Ajuste da página de detalhes tecnico

--- seguranca_comunitaria/urls.py
+++ seguranca_comunitaria/urls.py
@@ -18,7 +18,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
      # Inclui as URLs na raiz do site
@@ -26,3 +27,6 @@ urlpatterns = [
     path('denuncia/', include('denuncias.urls')), # Inclui as URLs na raiz do site
     path('tecnico/', include('tecnico.urls')), # Inclui as URLs do app tecnico
 ] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
